a5a5ff4813123b818bb34be64e853c58dd63fc0d
TERA630 <tera.bcool.4475@gmail.com>
2018-11-23 07:34:54 +0900

    factoring sdcard was finished making button

--- app/src/main/kotlin/FileIO.kt
+++ app/src/main/kotlin/FileIO.kt
@@ -5,22 +5,9 @@ import android.util.Log
 import androidx.documentfile.provider.DocumentFile
 import java.io.*
 
-
-fun isDirectoryFileContain(_fileList: Array<File>): Boolean {
-    var foundFlag = false
-    for (index in _fileList.indices) {
-        if (_fileList[index].name == TODO_TEXT_FILE) {
-            foundFlag = true
-        }
-    }
-    return foundFlag
-}
-
 fun loadListFromTextFile(context: Context): MutableList<ToDoItem> {
-
     val titleAndTagMatcher = "^(.+?),(.+?)(,.*)".toRegex()
     val allLine = readAllLineOfTextFile(context)
-
     var result = mutableListOf<ToDoItem>()
     for (i in allLine.indices) {
         var line = allLine[i]
@@ -34,35 +21,37 @@ fun loadListFromTextFile(context: Context): MutableList<ToDoItem> {
     return result
 }
 
-fun loadListFromTextFileAtSdcard(_context: Context, dir: DocumentFile): MutableList<ToDoItem> {
+fun loadListFromTextFileAtSdcard(_context: Context, _documentDir: DocumentFile): MutableList<ToDoItem> {
     val titleAndTagMatcher = "^(.+?),(.+?)(,.*)".toRegex()
     val result: MutableList<ToDoItem> = mutableListOf()
 
     try {
-        val file = dir.findFile(TODO_TEXT_FILE)
-        val ins = InputStreamReader(_context.contentResolver.openInputStream(file?.uri))
-        val br = BufferedReader(ins)
-        val AllLine = br.readLines()
-        for (i in AllLine.indices) {
-            var line = AllLine[i]
+        val file = _documentDir.findFile(TODO_TEXT_FILE)
+        file?.let {
+            val ins = InputStreamReader(_context.contentResolver.openInputStream(file.uri))
+            val br = BufferedReader(ins)
+            val allLine = br.readLines()
+            br.close()
+            for (i in allLine.indices) {
+                var line = allLine[i]
             titleAndTagMatcher.matchEntire(line)
                     ?.destructured
                     ?.let { (titleStr, tag, subProperty) ->
                         val newItem = subPropertyExtractFromText(subProperty, ToDoItem(title = titleStr, tagString = tag))
                         result.add(newItem)
                     }
-        }
-        return result
+            }
+            return result
+        } ?: throw Exception(FileNotFoundException("$TODO_TEXT_FILE was not found"))
     } catch (e: Exception) {
         e.printStackTrace()
     }
     return result
 }
 
-fun readAllLineOfTextFile(context: Context): List<String> {
+fun readAllLineOfTextFile(_context: Context): List<String> {
     return try {
-        val fileIn = context.openFileInput(TODO_TEXT_FILE)
-        val isr = InputStreamReader(fileIn)
+        val isr = InputStreamReader(_context.openFileInput(TODO_TEXT_FILE))
         val br = BufferedReader(isr)
         val result = br.readLines()
         br.close()
@@ -92,4 +81,3 @@ fun saveListToTextFile(context: Context, _list: MutableList<ToDoItem>) {
         e.printStackTrace()
     }
 }
-
