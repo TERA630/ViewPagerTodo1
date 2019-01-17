7e50486bb88c6284c1a251d46fd8e79b8d2137dc
TERA630 <tera.bcool.4475@gmail.com>
2019-01-17 21:17:37 +0900

    fix string resourse

--- app/src/main/kotlin/LoginActivity.kt
+++ app/src/main/kotlin/LoginActivity.kt
@@ -8,6 +8,10 @@ import com.dropbox.core.android.Auth
 import com.dropbox.core.v2.DbxClientV2
 import com.dropbox.core.v2.users.FullAccount
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import java.io.IOException
 
 const val DROPBOX_TOKEN = "dropbox_access_token"
@@ -21,48 +25,52 @@ class LoginActivity : AppCompatActivity() {
         val token = loadStringFromPreference(DROPBOX_TOKEN, this@LoginActivity.applicationContext)
         val signInButton = sign_in_button
         if (token == null) {
-            signInButton.setOnClickListener {
-                Auth.startOAuth2Authentication(applicationContext, getString(com.example.yoshi.viewpagertodo1.R.string.DROPBOX_APP_KEY))
-            }
+            signInButton.isEnabled = true
+            signInButton.setOnClickListener { Auth.startOAuth2Authentication(applicationContext, getString(com.example.yoshi.viewpagertodo1.R.string.DROPBOX_APP_KEY)) }
+            status_login.text = getString(R.string.status_not_login)
         } else {
-            val clientV2 = getDropBoxClient(token)
-            if (canClientLinkedName(clientV2)) {
-                status_login.text = getString(R.string.status_login, "")
-                val action = this.intent.getIntExtra("action", 0)
-                accessToDropBox(action, clientV2)
-            } else throw IllegalStateException("Token was not linked to Account.")
-        }
+            signInButton.isEnabled = false
+            val (client, userName) = convertTokenToClientValidated(token)
+            userName?.let {
+                status_login.text = getString(R.string.status_login, it)
+                val action = intent.getIntExtra("action", 0)
+                accessToDropBox(action, client)
+            }
+        } ?: errorFinishActivity("fail to establish client connection at onCreate")
     }
 
     override fun onResume() {
         super.onResume()
-        startAccessWithToken()
+        startAccessDBxWithAuth()
     }
 
-    private fun startAccessWithToken() {
+    private fun startAccessDBxWithAuth() {
         val accessToken = Auth.getOAuth2Token() //generate Access Token
         if (accessToken != null) {
             saveStringToPreference(DROPBOX_TOKEN, accessToken, this@LoginActivity.applicationContext)
-
             val currentUid = Auth.getUid()
             status_login.text = getString(R.string.status_login, currentUid)
             Log.i("test","$currentUid was logged in")
+
             val storedUid = loadStringFromPreference("user-id", this@LoginActivity.applicationContext)
             storedUid?.let {
                 if (currentUid != storedUid) saveStringToPreference("user-id", currentUid, this@LoginActivity.applicationContext)
             }
-            val clientV2 = getDropBoxClient(accessToken)
-            //Proceed to upload or download
-            if (!canClientLinkedName(clientV2)) {
-                Log.w("test", "Fail to get access_token")
-                setResult(Activity.RESULT_CANCELED)
-                finish()
-            } else {
+            val (clientV2, name) = convertTokenToClientValidated(accessToken)
+            if (name != null) {
                 val action = this.intent.getIntExtra("action", 0)
                 accessToDropBox(action, clientV2)
                 setResult(Activity.RESULT_OK)
+            } else {
+                errorFinishActivity("fail to get access_token at startAccesssDropboxafterAuth")
             }
         }
+    }
+
+    private fun convertTokenToClientValidated(token: String): Pair<DbxClientV2, String?> {
+        val clientV2 = getDropBoxClient(token)
+        val nameOfClient = canClientLinkedName(clientV2)?.name?.displayName
+        return Pair(clientV2, nameOfClient)
     }
 
     private fun accessToDropBox(request: Int, client: DbxClientV2): Boolean {
@@ -76,7 +84,6 @@ class LoginActivity : AppCompatActivity() {
                             Log.i("test", "upload succeeded.")
                             status_login.text = getString(R.string.status_complete_upload, TODO_TEXT_FILE)
                         }
-
                         override fun onError(error: Exception?) {
                             Log.w("test", "${error?.message} occur at accessToDropBox#UPLOAD")
                             throw IOException()
@@ -122,9 +129,27 @@ class LoginActivity : AppCompatActivity() {
         return false
     }
 
-
-    private fun canClientLinkedName(client: DbxClientV2): Boolean {
+    private fun canClientLinkedName(client: DbxClientV2): FullAccount? {
         try {
+            runBlocking {
+                val deferredAccount = GlobalScope.async(Dispatchers.Default) {
+                    client.users().currentAccount
+                }
+                return@runBlocking deferredAccount.await()
+            }
+        } catch (e: Exception) {
+            errorFinishActivity("client could not linked User name")
+        }
+        return null
+    }
+
+    private fun errorFinishActivity(message: String) {
+        Log.w("test", message)
+        setResult(Activity.RESULT_CANCELED)
+        finish()
+    }
+/*
+}
             val delegate = object : UserAccountTask.TaskDelegate {
                 override fun onAccountReceived(account: FullAccount) {
                     Log.i("test", "${account.name.displayName} was linked")
@@ -140,6 +165,5 @@ class LoginActivity : AppCompatActivity() {
             return true
         } catch (e: Exception) {
             return false
-        }
-    }
+        }*/
 }
